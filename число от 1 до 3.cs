357c1ba2055f48536b77e64a958ecba46b7f2fea
Максим Шабельник <92468591+Ssshaba@users.noreply.github.com>
2021-10-14 19:21:11 +0300

    Add files via upload

---
+++ число от 1 до 3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace число_от_1_до_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+            if ((a >= 1) && (a <= 3))
+            {
+                Console.WriteLine(a);
+            }
+            if ((b >= 1) && (b <= 3))
+            {
+                Console.WriteLine(b);
+            }
+            if ((c >= 1) && (c <= 3))
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
