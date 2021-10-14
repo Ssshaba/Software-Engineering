c1eb5eaa46baa48c1d6d001342207297d53bbc41
Максим Шабельник <92468591+Ssshaba@users.noreply.github.com>
2021-10-14 19:20:51 +0300

    Add files via upload

---
+++ дни недели.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace дни_недели
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            a = Convert.ToDouble(Console.ReadLine());
+            if (a == 1)
+            {
+                Console.WriteLine("Понедельник");
+            }
+            if (a == 2)
+            {
+                Console.WriteLine("Вторник");
+            }
+            if (a == 3)
+            {
+                Console.WriteLine("Среда");
+            }
+            if (a == 4)
+            {
+                Console.WriteLine("Четверг");
+            }
+            if (a == 5)
+            {
+                Console.WriteLine("Пятница");
+            }
+            if (a == 6)
+            {
+                Console.WriteLine("Суббота");
+            }
+            if (a == 7)
+            {
+                Console.WriteLine("Воскресенье");
+            }
+            if ((a > 7) || (a < 1))
+            {
+                Console.WriteLine("Введите от 1 до 7");
+            }
+        }
+    }
+}
