1017e0653a6149a9faefe4c07aa686988d8599b6
Максим Шабельник <92468591+Ssshaba@users.noreply.github.com>
2021-10-14 18:40:14 +0300

    Add files via upload

---
+++ макс 3 чисел.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Макс_3_чисел
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Math.Max(a, Math.Max(b, c)));
+
+
+
+        }
+    }
+}
