bfadebe5699076c3e4965a406c27b928400cb8da
Максим Шабельник <92468591+Ssshaba@users.noreply.github.com>
2021-10-14 19:22:05 +0300

    Add files via upload

---
+++ цена со скидкой.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace цена_со_скидкой
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int price, price_end;
+            price_end = 1;
+            price = Convert.ToInt32(Console.ReadLine());
+            if (price >= 500 && price < 1000)
+            {
+                price_end = price * 97 / 100;
+            }
+            if (price >= 1000)
+            {
+                price_end = price * 95 / 100;
+            }
+            if (price_end < 500)
+            {
+                price_end = price;
+            }
+            Console.WriteLine(price_end);
+        }
+    }
+}
