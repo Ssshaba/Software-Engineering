065cd1f00bbc7391e42a7ef991b3c852dbc6aaa0
Максим Шабельник <92468591+Ssshaba@users.noreply.github.com>
2021-10-20 17:23:44 +0300

    Add files via upload

---
+++ таблица умножения от 2 до 6.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace таблица_умножения
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j; // i-- на что умножают  j--что умножают 
+            for (i = 1; i <= 10; i++)
+            {
+                for (j = 2; j <= 6; j++)
+                {
+                    Console.Write($"{j} * {i} = {j *i}\t");
+                }
+                Console.WriteLine();
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
