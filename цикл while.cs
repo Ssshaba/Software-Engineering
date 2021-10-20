a444f0c1e68224572182f5ff79de7bda456ad259
Максим Шабельник <92468591+Ssshaba@users.noreply.github.com>
2021-10-20 16:56:08 +0300

    Add files via upload

---
+++ цикл while.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace цикл_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p, f = 1, i = 1;
+            p = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                f *= i;
+                i++;
+            }
+            while (i <= p);
+            Console.WriteLine("факториал = {0}",f);
+            Console.ReadLine();
+
+        }
+       
+    }
+}
