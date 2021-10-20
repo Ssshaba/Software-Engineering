a87c91b1da2e2b944f1af9ba8bcd02ef02e15cf1
Максим Шабельник <92468591+Ssshaba@users.noreply.github.com>
2021-10-20 16:56:55 +0300

    Add files via upload

---
+++ треугольник.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace треугольник
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int st = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= st; i++) 
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write('*');
+                    
+
+                    
+                }
+                Console.WriteLine();
+                
+            }
+            Console.ReadLine();
+        }
+        
+        
+    }
+}
