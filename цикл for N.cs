f36cb67d9ef80c361535939b2cedb6d77c94035a
Максим Шабельник <92468591+Ssshaba@users.noreply.github.com>
2021-10-20 16:55:29 +0300

    Add files via upload

---
+++ цикл for N.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace цикл_for_N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double N;
+            int summ;
+            summ = 0;
+            N = Convert.ToDouble(Console.ReadLine());
+            for ( int i = 1 ; i <= N ; i++)
+            {
+                summ += i;
+            }
+            Console.WriteLine("сумму первых {0} членов арифметической прогрессии = {1}", N, summ);
+            Console.ReadLine();
+        }
+    }
+}
